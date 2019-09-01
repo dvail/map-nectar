394f43adf039c8c7cee87b601bc81e852511e786
David Vail <dcvail17@gmail.com>
2019-09-01 10:03:48 -0400

    Reduce nesting, add more streams

--- src/renderPane.js
+++ src/renderPane.js
@@ -20,8 +20,8 @@ function RenderPane(initialVnode) {
   let viewport
   let skeletonGrid
   let hexGrid
-  let shiftDragCoords
 
+  let shiftDragCoords = stream(null)
   let dragging = stream(false)
   let shiftKey = stream(initialState.shiftKey)
   let mapData = stream(initialState.mapData)
@@ -54,8 +54,8 @@ function RenderPane(initialVnode) {
   function onDragStart(e) {
     let { x, y } = e.data.global
 
-    if (!shiftDragCoords) {
-      shiftDragCoords = { x, y }
+    if (!shiftDragCoords()) {
+      shiftDragCoords({ x, y })
     }
   }
 
@@ -65,20 +65,19 @@ function RenderPane(initialVnode) {
 
     let { x, y } = e.data.global
 
-    if (!shiftDragCoords) {
-      shiftDragCoords = { x, y }
+    if (!shiftDragCoords()) {
+      shiftDragCoords({ x, y })
     }
 
-    let deltaX = x - shiftDragCoords.x
-    let deltaY = y - shiftDragCoords.y
+    let deltaX = x - shiftDragCoords().x
+    let deltaY = y - shiftDragCoords().y
 
     // TODO Configure these magic numbers?
     let xRotations = Math.round(deltaX / 40)
     let yRotations = Math.round(deltaY / 40)
 
     if (xRotations || yRotations) {
-      shiftDragCoords.x = x
-      shiftDragCoords.y = y
+      shiftDragCoords({ x, y })
     }
 
     if (xRotations < 0) {
@@ -95,74 +94,76 @@ function RenderPane(initialVnode) {
   }
 
   function onDragEnd() {
-    shiftDragCoords = null
+    shiftDragCoords(null)
+  }
+
+  function oncreate(vnode) {
+    let paneElem = vnode.dom
+    app = new PIXI.Application({ resizeTo: paneElem })
+    paneElem.appendChild(app.view)
+
+    viewport = new Viewport({ interaction: app.renderer.plugins.interaction })
+
+    app.stage.addChild(viewport)
+
+    viewport.drag().wheel()
+    viewport.on('drag-start', () => dragging(true))
+    viewport.on('drag-end', () => dragging(false))
+    viewport.moveCenter(275, 50) // TODO These are magic values...
+
+    skeletonGrid = HexagonGrid.create({ ...gridLayoutOps, onTileClick, onTileRightClick })
+
+    hexGrid = HexagonGrid.create({ ...gridLayoutOps, onTileClick, onTileRightClick })
+    // TODO The performance of this probably sucks
+    _.range(-10, 10).forEach(q => {
+      _.range(-10, 10).forEach(r => {
+        skeletonGrid.renderTile({ q, r, height: 0, opts: skeletonTileOpts })
+      })
+    })
+
+    viewport.addChild(skeletonGrid.container)
+
+    viewport.on('pointerdown', onDragStart)
+      .on('pointerup', onDragEnd)
+      .on('pointerupoutside', onDragEnd)
+      .on('pointermove', onDragMove)
+
+    viewport.addChild(hexGrid.container)
+  }
+
+  function onbeforeupdate(vnode, old) {
+    let newState = vnode.attrs.state
+    let oldState = old.attrs.state
+
+    mapData(newState.mapData)
+    shiftKey(newState.shiftKey)
+
+    // TODO React to state.viewAngle
+
+    // TODO Convert this section to make better use of streams?
+    if (newState.rotation !== oldState.rotation) {
+      hexGrid?.setRotation(newState.rotation)
+      skeletonGrid?.setRotation(newState.rotation)
+    }
+
+    if (newState.shiftKey) {
+      viewport?.plugins.pause('drag')
+    } else {
+      viewport?.plugins.resume('drag')
+    }
+
+    // Note: This relies on an object reference change, since data updates
+    // are immutable, the object reference changing indicates a new set of map
+    // tiles.
+    if (newState.mapData.tiles !== oldState.mapData.tiles) {
+      hexGrid?.renderTiles(newState.mapData.tiles)
+    }
   }
 
   return {
-    oncreate: vnode => {
-      let paneElem = vnode.dom
-      app = new PIXI.Application({ resizeTo: paneElem })
-      paneElem.appendChild(app.view)
-
-      viewport = new Viewport({ interaction: app.renderer.plugins.interaction })
-
-      app.stage.addChild(viewport)
-
-      viewport.drag().wheel()
-      viewport.on('drag-start', () => dragging(true))
-      viewport.on('drag-end', () => dragging(false))
-      viewport.moveCenter(275, 50) // TODO These are magic values...
-
-      skeletonGrid = HexagonGrid.create({ ...gridLayoutOps, onTileClick, onTileRightClick })
-
-      hexGrid = HexagonGrid.create({ ...gridLayoutOps, onTileClick, onTileRightClick })
-      // TODO The performance of this probably sucks
-      _.range(-10, 10).forEach(q => {
-        _.range(-10, 10).forEach(r => {
-          skeletonGrid.renderTile({ q, r, height: 0, opts: skeletonTileOpts })
-        })
-      })
-
-      viewport.addChild(skeletonGrid.container)
-
-      viewport.on('pointerdown', onDragStart)
-        .on('pointerup', onDragEnd)
-        .on('pointerupoutside', onDragEnd)
-        .on('pointermove', onDragMove)
-
-      viewport.addChild(hexGrid.container)
-    },
-    onbeforeupdate: (vnode, old) => {
-      let newState = vnode.attrs.state
-      let oldState = old.attrs.state
-
-      mapData(newState.mapData)
-      shiftKey(newState.shiftKey)
-
-      // TODO React to state.viewAngle
-
-      // TODO Convert this section to make better use of streams?
-      if (newState.rotation !== oldState.rotation) {
-        hexGrid?.setRotation(newState.rotation)
-        skeletonGrid?.setRotation(newState.rotation)
-      }
-
-      if (newState.shiftKey) {
-        viewport?.plugins.pause('drag')
-      } else {
-        viewport?.plugins.resume('drag')
-      }
-
-      // Note: This relies on an object reference change, since data updates
-      // are immutable, the object reference changing indicates a new set of map
-      // tiles.
-      if (newState.mapData.tiles !== oldState.mapData.tiles) {
-        hexGrid?.renderTiles(newState.mapData.tiles)
-      }
-    },
-    view: () => (
-      m('.renderPane')
-    ),
+    oncreate,
+    onbeforeupdate,
+    view: () => m('.renderPane'),
   }
 }
 
