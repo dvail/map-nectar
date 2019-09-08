f885e6da912bf2763eb951af4c938661ff415e4a
David Vail <dcvail17@gmail.com>
2019-09-07 21:31:25 -0400

    More style cleanup.

--- src/components/atlasRegion.js
+++ src/components/atlasRegion.js
@@ -1,20 +1,21 @@
 import m from 'mithril'
+import { tw } from '../util'
+
+let RegionStyle = tw`transform-origin-tl bg-no-repeat`
 
 let Region = {
   view: ({ attrs: { image, atlas, region, scale = 1.0 } }) => {
     let { x, y, w, h } = atlas[region]
 
-    let regionStyle = {
+    let style = {
       height: `${h}px`,
       width: `${w}px`,
       transform: `scale(${scale})`,
-      transformOrigin: 'top left',
-      backgroundRepeat: 'no-repeat',
       backgroundImage: `url('${image}')`,
       backgroundPosition: `-${x}px -${y}px`,
     }
 
-    return m('div', { style: regionStyle })
+    return m(RegionStyle, { style })
   },
 }
 
@@ -22,12 +23,12 @@ let RegionWrapper = {
   view: ({ attrs, attrs: { atlas, region, scale = 1.0 } }) => {
     let { w, h } = atlas[region]
 
-    let wrapperStyle = {
+    let style = {
       height: `${h * scale}px`,
       width: `${w * scale}px`,
     }
 
-    return m('div', { style: wrapperStyle }, m(Region, attrs))
+    return m('div', { style }, m(Region, attrs))
   },
 }
 
